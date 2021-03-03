c53d6ee096243f702ae2fe7aa4ca00d9b310bd7f
zhangzheng <zhangzheng@olecx.com>
2021-03-03 17:27:07 +0800

    feat：添加206题

--- src/LeetCode.kt
+++ src/LeetCode.kt
@@ -1,8 +1,3 @@
-import Array.removeElement
-import Linked_List.ListNode
-import Linked_List.removeElements
-import Linked_List.removeElementsForRecursion
-
 /**
  * @author zhangzheng
  * @Date  2021/1/10 下午4:29
@@ -10,22 +5,7 @@ import Linked_List.removeElementsForRecursion
  * <p>
  * Desc :测试
  */
-
 fun main() {
-    val l1 = ListNode(1)
-    val l2 = ListNode(2)
-    val l3 = ListNode(6)
-    val l4 = ListNode(5)
-    val l5 = ListNode(4)
-    val l6 = ListNode(6)
-    l1.next = l2
-    l2.next = l3
-    l3.next = l4
-    l4.next = l5
-    l5.next = l6
-    l6.next = null
-    removeElementsForRecursion(l1, 6)
 }
-
 
 
---
+++ src/Linked_List/206.Reverse_Linked_List.kt
@@ -0,0 +1,36 @@
+package Linked_List
+
+/**
+ * @author zhangzheng
+ * @Date  2021/3/3 下午5:21
+ * @ClassName `206.Reverse_Linked_List`
+ * <p>
+ * Desc :反转链表
+ */
+//反转一个单链表。
+//
+// 示例:
+//
+// 输入: 1->2->3->4->5->NULL
+//输出: 5->4->3->2->1->NULL
+//
+// 进阶:
+//你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+// Related Topics 链表
+// 👍 1544 👎 0
+
+
+/**
+ * 双指针方法
+ */
+fun reverseList(head: ListNode?): ListNode? {
+    var left: ListNode? = null
+    var right = head
+    while (right != null) {
+        val temp = right.next
+        right.next = left
+        left = right
+        right = temp
+    }
+    return left
+}
