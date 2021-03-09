2e8aed6332e0fcc51a9470efa3eda4243c6bf257
zhangzheng <zhangzheng@olecx.com>
2021-03-09 17:45:12 +0800

    feat：添加数组实现的Queue类

---
+++ src/Queue/MyQueue.kt
@@ -0,0 +1,71 @@
+package Queue
+
+/**
+ * @author zhangzheng
+ * @Date  2021/3/9 下午5:19
+ * @ClassName MyQueue
+ * <p>
+ * Desc : 通过数组来实现一个队列结构
+ */
+class MyQueueForArray(size: Int) {
+    var queArray: Array<Any?>
+    var maxSize = 0
+    var front = 0
+    var rear = -1
+    var nItems = 0
+
+    init {
+        maxSize = size
+        queArray = arrayOf(maxSize)
+    }
+
+    //入队操作
+    fun enqueue(value: Any): Boolean {
+        return if (isFull()) {
+            false
+        } else {
+            if (rear == (maxSize - 1)) {
+                //如果已经指到最后一个了
+                rear = -1
+            }
+            queArray[++rear] = value
+            nItems++
+            true
+        }
+    }
+
+    //出队操作
+    fun dequeue(): Any? {
+        var dequeueValue: Any? = null
+        if (!isEmpty()) {
+            dequeueValue = queArray[front]
+            queArray[front] = null
+            front++
+            if (front == maxSize) {
+                front = 0
+            }
+            nItems--
+        }
+        return dequeueValue
+    }
+
+    //查看当前队列第一个元素
+    fun peekFront(): Any? {
+        return queArray[front]
+    }
+
+    //是否已经填满了
+    fun isFull(): Boolean {
+        return maxSize == nItems
+    }
+
+    //是否为空队列
+    fun isEmpty(): Boolean {
+        return nItems == 0
+    }
+
+    //队列长度
+    fun getSize(): Int {
+        return nItems
+    }
+}
